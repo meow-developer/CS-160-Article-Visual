35589d83ea11725534d59850017af4cc5249e6be
leeanson19 <leeanson19@gmail.com>
2024-05-04 18:16:04 -0700

    Fix url param missing

--- src/middleware/validator/article.ts
+++ src/middleware/validator/article.ts
@@ -1,11 +1,8 @@
 import { param, query } from "express-validator";
-import ArticleDb from "../../repo/articleDb.js";
-import ValidationRestError from "./ownValidatorError.js";
-
-
 
 export const articleIdValidator = param('articleId')
                                 .isInt({min: 1})
+                                .withMessage('Article ID must be a positive integer');
 
 export const articleQueryValidator = query('limit')
                                     .isInt({min: 1})
